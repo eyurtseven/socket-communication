a1644ad3743e7bba45283e016b8fd49a9108cce1
eyurtseven <ben@emreyurtseven.com>
2017-10-24 08:59:15 +0300

    port changes

--- app.js
+++ app.js
@@ -55,8 +55,8 @@ io.on('connection', function(socket) {
 
 });
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
+http.listen(8000, function() {
+    console.log('listening on *:8000');
 });
 
 
